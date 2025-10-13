db3c70063c2f1e3b35e4999e663590c46d2879f3
dimetriLee <dimetrialee@students.utech.edu.jm>
2025-10-13 14:16:08 -0500

    dashboard

--- tipme/src/App.tsx
+++ tipme/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import AuthPage from "./pages/Auth";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         {/* Landing Page as the default route */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<AuthPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
 
         {/* Other routes */}
       </Routes>
